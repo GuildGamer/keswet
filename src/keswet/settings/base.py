7696dc1a8f2349bd16f0977f53bbab18c8339eb3
nurulmasih <tobi4steve@gmail.com>
2022-05-04 20:23:14 +0100

    heroku fix

--- src/keswet/settings/base.py
+++ src/keswet/settings/base.py
@@ -13,11 +13,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 from decouple import config
 import sys
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -143,3 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+import django_heroku
+
+django_heroku.settings(locals())
